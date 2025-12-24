600443f76b384c29e39e9a0000f36876e73e8302
dvardoo <edvard.tengqvist@protonmail.com>
2025-12-24 01:15:12 +0100

    Workstations module

--- hosts/nix-thinkpad/configuration.nix
+++ hosts/nix-thinkpad/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/common.nix
+      ../../modules/workstations.nix
       inputs.home-manager.nixosModules.default
     ];
 
@@ -26,59 +27,11 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Unlock disk GUI, also quiet systemd startup messages
-  #boot = {
-    # silence first boot output
-  #  consoleLogLevel = 3;
-  #  initrd.verbose = false;
-  #  initrd.systemd.enable = true;
-  #  kernelParams = [
-  #      "quiet"
-  #      "splash"
-  #      "intremap=on"
-  #      "boot.shell_on_fail"
-  #      "udev.log_priority=3"
-  #      "rd.systemd.show_status=auto"
-  # ];
-
-    # plymouth, showing after LUKS unlock
-  #  plymouth.enable = true;
-  #  plymouth.font = "${pkgs.hack-font}/share/fonts/truetype/Hack-Regular.ttf";
-  #  plymouth.logo = "${pkgs.nixos-icons}/share/icons/hicolor/128x128/apps/nix-snowflake.png";
-  #};
-
-  # Performance tuning
-  boot.kernelPackages = pkgs.linuxPackages_zen; 
-  #services.bpftune.enable = true; # Enable the bpftune service
-  #services.bpftune.package = pkgs.bpftune;  # Specify the bpftune package
-
   # hashcat / games
   #hardware.graphics.extraPackages = with pkgs; [
   #  rocmPackages.clr.icd
   #];
 
-  # Enable COSMIC DE
-  services.displayManager.cosmic-greeter.enable = true;
-  services.desktopManager.cosmic.enable = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dvardo = {
@@ -107,7 +60,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   #  nix-search-cli
     keepassxc
---
+++ modules/workstations.nix
@@ -0,0 +1,83 @@
+{ config, pkgs, inputs, ... }:
+{
+  # Unlock disk GUI, also quiet systemd startup messages
+  #boot = {
+    # silence first boot output
+  #  consoleLogLevel = 3;
+  #  initrd.verbose = false;
+  #  initrd.systemd.enable = true;
+  #  kernelParams = [
+  #      "quiet"
+  #      "splash"
+  #      "intremap=on"
+  #      "boot.shell_on_fail"
+  #      "udev.log_priority=3"
+  #      "rd.systemd.show_status=auto"
+  # ];
+
+    # plymouth, showing after LUKS unlock
+  #  plymouth.enable = true;
+  #  plymouth.font = "${pkgs.hack-font}/share/fonts/truetype/Hack-Regular.ttf";
+  #  plymouth.logo = "${pkgs.nixos-icons}/share/icons/hicolor/128x128/apps/nix-snowflake.png";
+  #};
+
+  # Performance tuning
+  boot.kernelPackages = pkgs.linuxPackages_zen;
+  #services.bpftune.enable = true; # Enable the bpftune service
+  #services.bpftune.package = pkgs.bpftune;  # Specify the bpftune package
+
+# Enable COSMIC DE
+  services.displayManager.cosmic-greeter.enable = true;
+  services.desktopManager.cosmic.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  wget
+  #  nix-search-cli
+    alacritty
+    keepassxc
+    authenticator
+    veracrypt
+    nextcloud-client
+    wireguard-tools
+    kitty
+    tmux
+    hashcat
+    nmap
+    #bpftune # Not working?
+    #flameshot
+    #signal check up
+    discord
+    #steam needs service?
+    #lutris check up
+    #heroiclauncher check up
+    #somekindofminecraft launcher ?
+    #clamscan?
+    virt-viewer
+    scrcpy
+    vscodium-fhs
+   # Unstable packages
+   #unstable.packagename
+    #unstable.srccpy
+  ];
+}
