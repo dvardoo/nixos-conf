0e27fafdf809b2df4cfcb48121625b1e43c7f0fb
dvardoo <edvard.tengqvist@protonmail.com>
2025-12-27 01:18:44 +0100

    Cleanup

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -7,12 +7,5 @@
       KbdInteractiveAuthentication = false;
       PermitRootLogin = "no";
     };
-    authorizedKeys = {
-      dvardo = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF9egc+MNLkaP9pnz0G0kUt2Bb5HOeJTgAuUXOzK0T1e workstation"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKsqdLfJFkI3rTRpG0y4r80j3fAV+VVgkZJgqu0v3dra nix-thinkpad"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDSyxN4GOtbvU++XRFRe2XtcksFUIE3OEPWg6ptEF7+n pixel"
-      ];
-    };
   };
 }
