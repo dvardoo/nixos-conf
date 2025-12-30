3979e9011271b74ba82228cde7927ddf5f1d7e30
dvardoo <edvard.tengqvist@protonmail.com>
2025-12-30 22:14:22 +0100

    Added fish shell

--- modules/user.nix
+++ modules/user.nix
@@ -1,10 +1,13 @@
 { config, pkgs, inputs, ... }:
 {
+  programs.fish.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dvardo = {
     isNormalUser = true;
     description = "dvardo";
     extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
     packages = with pkgs; [
     #  thunderbird
     ];
