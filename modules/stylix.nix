ef6b4c93e3c8deb5f2cc564f745e20f6e8978b64
dvardoo <edvard.tengqvist@protonmail.com>
2026-01-21 00:22:58 +0100

    Changed to gruvbox

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -4,12 +4,12 @@
   stylix = {
     enable = true;
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     
     # Use the override option for color customization
-    override = {
-      base0D = "#A6E3A1";  # green accent
-    };
+    #override = {
+    #  base0D = "#A6E3A1";  # green accent
+    #};
   };
 }
