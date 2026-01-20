32d367aa02ce3ff0c16f026734c5c3eecdb29cd5
dvardoo <edvard.tengqvist@protonmail.com>
2026-01-21 00:39:50 +0100

    Changed to gruvbox-dark-medium

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -5,7 +5,7 @@
     enable = true;
     polarity = "dark";
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
     
     # Use the override option for color customization
     #override = {
