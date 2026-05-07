20f25cc213f0bf436ea7751758354acdbd50a656
dvardo <67321210+dvardoo@users.noreply.github.com>
2026-05-07 21:40:47 +0200

    WIP: added default-lite.nix

---
+++ modules/default-lite.nix
@@ -0,0 +1,18 @@
+{ ... }:
+
+{
+  imports =
+    [
+      ./user.nix
+      ./shell.nix
+      ./common.nix
+      #./workstations.nix
+      #./gaming.nix
+      ./gc.nix
+      #./amdrocm.nix
+      #./llm.nix
+      #./stylix.nix
+      ./overlays.nix
+    ];
+
+}
