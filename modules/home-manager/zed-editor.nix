5b06911e357b8b96c4f00f2b889a8a6752bb8374
dvardo <67321210+dvardoo@users.noreply.github.com>
2026-05-08 21:52:14 +0200

    added zed-editor

--- modules/home-manager/zed-editor.nix
+++ modules/home-manager/zed-editor.nix
@@ -5,11 +5,6 @@
     enable = true;
     extensions = [ "nix" ];
     userSettings = {
-    #  theme = {
-    #    mode = "system";
-    #    dark = "One Dark";
-    #    light = "One Light";
-    #  };
       hour_format = "hour24";
       vim_mode = true;
     };
