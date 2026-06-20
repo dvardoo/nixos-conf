0ba5ef18cc6b4e49499a2a07fba4fb41430cfc22
dvardoo <edvard.tengqvist@protonmail.com>
2026-06-20 22:16:47 +0200

    update alias

--- modules/shell.nix
+++ modules/shell.nix
@@ -20,7 +20,7 @@
       la = "eza -la --icons --group-directories-first";
       tree = "eza --icons --tree --group-directories-last";
       needs-reboot = ''if diff <(readlink /run/booted-system/{initrd,kernel,kernel-modules}) <(readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules}); then echo "✓ No reboot needed"; else echo "↻ Reboot needed"; fi'';
-      update = "cd ~/nixos-conf/ && git pull && sudo nixos-rebuild switch --flake .#$(hostname)";
+      update = "cd ~/nixos-conf/ && git pull && sudo nixos-rebuild switch --flake .#$(hostname) && nvd diff /run/booted-system /nix/var/nix/profiles/system && needs-reboot";
       open = "xdg-open";
     };
 
