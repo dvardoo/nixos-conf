e287fdc58542d27b727fecb45a42d46f04da905a
dvardoo <edvard.tengqvist@protonmail.com>
2026-06-30 07:51:34 +0200

    changed zen -> xanmod

--- modules/performance.nix
+++ modules/performance.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
   # Performance tuning
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  #boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
   #services.bpftune.enable = true; # Enable the bpftune service
   #services.bpftune.package = pkgs.bpftune;  # Specify the bpftune package
   services.system76-scheduler.enable = true;
