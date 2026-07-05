03a027c7614e1c9d6c99a87eca9b12585d0d4e10
dvardoo <edvard.tengqvist@protonmail.com>
2026-07-06 01:28:03 +0200

    Fixed kanagawa-lotus. Added toggle-theme script and specialisation for dark mode

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,23 +1,22 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
-  isDarkMode = true;  # Switch to false for light mode
-
   darkTheme = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
 
   lightTheme = {
     scheme = "Kanagawa Lotus";
     author = "rebelot";
-    base00 = "f1f5f8";
+    variant = "light";
+    base00 = "f2ecbc";
     base01 = "e5ddb0";
     base02 = "dcd5ac";
     base03 = "c8a882";
     base04 = "8a8980";
-    base05 = "545464";
+    base05 = "43436c";
     base06 = "3d3b52";
     base07 = "1f1f28";
     base08 = "c84053";
     base09 = "cc6d00";
-    base0A = "77713f";
+    base0A = "e4c9af";
     base0B = "6f894e";
     base0C = "597b75";
     base0D = "4d699b";
@@ -25,12 +24,38 @@ let
     base0F = "b35b79";
   };
 
-  activeScheme = if isDarkMode then darkTheme else lightTheme;
+  toggleThemeScript = pkgs.writeShellScriptBin "toggle-theme" ''
+    COSMIC_FILE="$HOME/.config/cosmic/com.system76.CosmicTheme.Mode/v1/is_dark"
+    mkdir -p "$(dirname "$COSMIC_FILE")"
+
+    if [ ! -f "$COSMIC_FILE" ] || grep -q "false" "$COSMIC_FILE"; then
+      sudo /nix/var/nix/profiles/system/specialisation/dark/bin/switch-to-configuration switch
+      echo true > "$COSMIC_FILE"
+    else
+      sudo /nix/var/nix/profiles/system/bin/switch-to-configuration switch
+      echo false > "$COSMIC_FILE"
+    fi
+  '';
 in
 {
   stylix = {
     enable = true;
-    polarity = if isDarkMode then "dark" else "light";
-    base16Scheme = activeScheme;
+    polarity = "light";
+    base16Scheme = lightTheme;
+  };
+
+  environment.systemPackages = [ toggleThemeScript ];
+
+  home-manager.users.dvardo.xdg.configFile."gtk-3.0/gtk.css".force = true;
+  home-manager.users.dvardo.xdg.configFile."gtk-4.0/gtk.css".force = true;
+
+  specialisation = {
+    dark = {
+      inheritParentConfig = true;
+      configuration = {
+        stylix.polarity = lib.mkForce "dark";
+        stylix.base16Scheme = lib.mkForce darkTheme;
+      };
+    };
   };
 }
