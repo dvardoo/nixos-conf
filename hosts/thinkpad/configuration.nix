eac5e2e51bc1c35e11e13d4b391eae0a5d951c59
dvardoo <edvard.tengqvist@protonmail.com>
2026-07-06 22:27:35 +0200

    added laptop-power.nix for thinkpad

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -5,6 +5,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/default.nix
+      ../../modules/laptop-power.nix
       #inputs.home-manager-unstable.nixosModules.default
     ];
 
